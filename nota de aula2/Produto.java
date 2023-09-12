639d5a91419b7feb88647255a0ac5f16d4eb0ae2
jair <129990475+jairwictor@users.noreply.github.com>
2023-09-12 20:07:09 -0300

    add getter setter

--- nota de aula2/Produto.java
+++ nota de aula2/Produto.java
@@ -5,10 +5,51 @@ public class Produto {
 
     int quantidade_vender;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getQuantidade_vender() {
+        return quantidade_vender;
+    }
+
+    public void setQuantidade_vender(int quantidade_vender) {
+        this.quantidade_vender = quantidade_vender;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
+    }
+
     int resultado;
 
     public double fazer_vendas(int quantidade, int quantidade_vender){
         resultado = quantidade - quantidade_vender;
+        this.resultado = resultado;
         return resultado;
 
     }
